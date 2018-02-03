5f093da22a8bb3ec692d4feb21636364fcdddf19
Marcel Koonstra <marcel@abstraction.nl>
2018-02-03 17:20:30 +0100

    Upgrade to .netcore 2.0

--- src/Rehearsal.WebApi/Infrastructure/ServiceCollectionExtensions.cs
+++ src/Rehearsal.WebApi/Infrastructure/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -18,32 +19,33 @@ namespace Rehearsal.WebApi.Infrastructure
                     options.Filters.Add(new ValidateModelStateFilter());
                 })
                 .AddFluentValidation();
+
+            services.AddAuthentication(a => a.DefaultScheme = "Bearer")
+                .AddJwtBearer(configureOptions =>
+                {
+                    var jwtOptions = services.BuildServiceProvider().GetService<IOptions<JwtOptions>>().Value;
+
+                    configureOptions.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtOptions.Issuer,
+
+                        ValidateAudience = true,
+                        ValidAudience = jwtOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = jwtOptions.SigningCredentials.Key,
+
+                        RequireExpirationTime = true,
+                        ValidateLifetime = true,
+
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
         }
 
         public static void UseWebApi(this IApplicationBuilder app)
         {
-            var jwtOptions = app.ApplicationServices.GetService<IOptions<JwtOptions>>().Value;
-            
-            app.UseJwtBearerAuthentication(new JwtBearerOptions()
-            {
-                AutomaticAuthenticate = true,
-                AutomaticChallenge = true,
-                TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = jwtOptions.Issuer,
-
-                    ValidateAudience = true,
-                    ValidAudience = jwtOptions.Audience,
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = jwtOptions.SigningCredentials.Key,
-
-                    RequireExpirationTime = true,
-                    ValidateLifetime = true,
-
-                    ClockSkew = TimeSpan.Zero
-                }
-            });
+            app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
         }
     }
