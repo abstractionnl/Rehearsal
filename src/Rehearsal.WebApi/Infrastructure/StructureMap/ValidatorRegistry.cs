35749c57f9d1acb9e57493d6a44f4801c075b53f
Marcel Koonstra <marcel@abstraction.nl>
2018-03-30 18:00:05 +0200

    Fix component registration for FluentValidation

--- src/Rehearsal.WebApi/Infrastructure/StructureMap/ValidatorRegistry.cs
+++ src/Rehearsal.WebApi/Infrastructure/StructureMap/ValidatorRegistry.cs
@@ -9,7 +9,7 @@ namespace Rehearsal.WebApi.Infrastructure.StructureMap
         {
             Scan(_ =>
             {
-                _.TheCallingAssembly();
+                _.AssemblyContainingType(typeof(ValidatorRegistry));
                 _.ConnectImplementationsToTypesClosing(typeof(IValidator<>))
                     .OnAddedPluginTypes(x => x.Singleton()) // Validators are singletons, dependencies must be Lazy<T> or Func<T>
                 ;    
